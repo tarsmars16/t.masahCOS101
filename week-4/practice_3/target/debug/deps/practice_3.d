e5762e53c519571c4c29a10f8b6b69c5b1653cbf
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-10-22 16:32:07 +0100

    (Week 3 Practice 3)

---
+++ week-4/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\HCC\Documents\t.masahCOS101\week-4\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\HCC\Documents\t.masahCOS101\week-4\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
