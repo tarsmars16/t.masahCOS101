d87634584464475223d3d56ed97dec8fb29fb916
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-05 16:35:36 +0100

    (Week 6 Practice 5)

--- week-6/practice_5/src/main.rs
+++ week-6/practice_5/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let fullname = "Pan-Atlantic University";
+    println!();
+    println!("Name: {}",fullname);
+    println!();
+    println!("Before trim ");
+    println!("Length is {}",fullname.len());
+    println!();
+    println!("After trim ");
+    println!("Length is {}",fullname.trim().len());
+    
 }
