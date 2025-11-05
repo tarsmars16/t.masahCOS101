01a96229f94fe7c575a70d28c44b2d1489881ad3
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-05 17:20:06 +0100

    (Week 6 Practice 11)

--- week-6/practice_11/src/main.rs
+++ week-6/practice_11/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let a:i32 = 2; //Bit presentation 10
+    let b:i32 = 3; //Bit presentation 11
+
+    let mut result:i32;
+
+    result = a & b;
+    println!("(a & b) => {}",result);
+
+    result = a | b;
+    println!("(a | b) => {}",result);
+
+    result = a ^ b;
+    println!("(a ^ b) => {}",result);
+
+    result = !b;
+    println!("(|b) => {}",result);
+
+    result = a << b;
+    println!("(a << b) => {}",result);
+
+    result = a >> b;
+    println!("(a >> b) => {}",result);
 }
