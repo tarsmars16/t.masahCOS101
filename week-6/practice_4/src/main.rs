9eaf9c9221e8797630eb1bc45956aa80fe7de0f1
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-05 16:31:44 +0100

    (Week 6 Practice 4)

--- week-6/practice_4/src/main.rs
+++ week-6/practice_4/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let fullname = "Chidubum John Umeh";
+    let department = "Computer Science";
+    let uni = "Pan-Atlantic University";
+
+    let mut school = "School of Science".to_string();
+    //push string
+    school.push_str(" and Technology");
+
+    println!("My name is {}",fullname);
+    //check length
+    println!("The length of my fullname is {}",fullname.len());
+    println!("I am a student of {} Department",department);
+    println!("{}",school);
+    println!("{}",uni);
 }
