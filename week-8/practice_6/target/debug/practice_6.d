46022038ca6b80123a3b9041922a9c4975f0e0fc
tarsmars16 <tamaratare.masah@pau.edu.ng>
2025-11-12 20:29:20 +0100

    Delete week-8 directory

--- week-8/practice_6/target/debug/practice_6.d
+++
@@ -1,1 +0,0 @@
-C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_6\target\debug\practice_6.exe: C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_6\src\main.rs
--- week-8/practice_8iter/target/debug/practice_8iter.d
+++
@@ -1,1 +0,0 @@
-C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_8iter\target\debug\practice_8iter.exe: C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_8iter\src\main.rs
