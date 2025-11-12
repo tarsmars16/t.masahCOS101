55e93d6b32ad003573c2edd303fe76e0d41b3865
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-12 20:19:08 +0100

    (Returning value from a function)

--- week-7/practice_3/src/main.rs
+++ week-7/practice_3/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("The pi value is {}",get_pi());
+}
+// get pi
+fn get_pi()->f64 {
+    let a:f64 = 22.0;
+    let b:f64 = 7.0;
+    let c:f64 = a/b;
+    return c;
 }
