e6743d7175b5fa710ebc49ff51fe20cea979267c
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-12 19:44:38 +0100

    (Pass by Value)

--- week-7/practice_5/src/main.rs
+++ week-7/practice_5/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let num:i32 = 5;
     mutate_num_to_zero(num);
-    println!("The Value of no is: {}"num);
+    println!("The Value of no is: {}",num);
 
 }
 fn mutate_num_to_zero(mut param_num: i32) {
