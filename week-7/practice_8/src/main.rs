11f0e4bf3ddd1d6997248b1907c23f0409b41ffb
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-12 20:27:17 +0100

    (Array with for loop)

--- week-7/practice_8/src/main.rs
+++ week-7/practice_8/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let city_arr:[&str;5] = ["Abuja","PortHarcourt","Maiduguri","Kano","Lagos"];
+    println!("array is {:?}",city_arr);
+    println!("array size is: {}",city_arr.len());
+
+    for index in 0..5 {
+        println!("City index {} is located in: {}",index,city_arr[index]);
+    }
 }
