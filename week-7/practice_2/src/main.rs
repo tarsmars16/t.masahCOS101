9561c1ef991d7bcfee05676023bd53346d5e9ece
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-12 20:16:29 +0100

    (Invoking a Function 2)

--- week-7/practice_2/src/main.rs
+++ week-7/practice_2/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
+fn checker() {
+    let mut input = String::new();
+    println!("Enter a character:");
+    io::stdin().read_line(& mut input).expect("Failed to Read Input");
+    let ch:char = input.trim().parse().expect("Invalid Input");
+
+    if ch >= '0' && ch<= '9' {
+        println!("Character '{}' is a digit",ch);
+    }
+    else {
+        println!("Character '{}' is not a digit",ch);
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    //Calling function
+    println!("Welcome! This program checks whether a character variable contains a digit
+        or not");
+    checker()
 }
