94cc524078f1a29d2178e0294c801cf929b99b72
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-12 19:49:22 +0100

    (Array with for loop using iter() function)

---
+++ week-7/practice_8iter/target/debug/deps/practice_8iter.d
@@ -0,0 +1,5 @@
+C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_8iter\target\debug\deps\practice_8iter.d: src\main.rs
+
+C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_8iter\target\debug\deps\practice_8iter.exe: src\main.rs
+
+src\main.rs:
---
+++ week-7/practice_8iter/target/debug/practice_8iter.d
@@ -0,0 +1,1 @@
+C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_8iter\target\debug\practice_8iter.exe: C:\Users\HCC\Documents\t.masahCOS101\week-7\practice_8iter\src\main.rs
