81a2ac066d77ec6f9fff898edd26362641aedd73
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-20 16:03:30 +0100

    week-8

--- week-8/practice_2/target/debug/practice_2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_2\target\debug\practice_2.exe: C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_2\src\main.rs
--- week-8/practice_4/target/debug/practice_4.d
+++
@@ -1,1 +0,0 @@
-C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_4\target\debug\practice_4.exe: C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_4\src\main.rs
