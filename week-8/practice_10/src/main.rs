ad3a00d3d0483653c345fd404a95b8517145fc73
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-20 17:15:53 +0100

    Destructuring a tuple

--- week-8/practice_10/src/main.rs
+++ week-8/practice_10/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let b: (i32,bool,f64) = (30,true,4.9);
+    print(b)
+
+}
+
+fn print(x:(i32,bool,f64)) {
+    //assigns a tuple to distinct variables
+    let (age,is_male,cgpa) = x;
+    println!("Age is {} , isMale? {}, CGPA is {}",age,is_male,cgpa);
+    
 }
