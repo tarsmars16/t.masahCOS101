7e2eeba8aecf2e12363e3f506d33a7b9a68325b7
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-20 16:33:34 +0100

    Using get() method

--- week-8/practice_3/src/main.rs
+++ week-8/practice_3/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+//Methos to print the get value
+
+fn value(n:Option<&char>) {
+    println!("Element of vector {:?}",n);
+
 }
+
+fn main () {
+    let v = vec!['R','U','S','T','I','C','I','A','N'];
+
+    let mut input1 = String::new();
+    println!("\nEnter an Index value btw (0 - 8)");
+    std::io::stdin().read_line(&mut input1).expect("Failed to Read Input");
+
+    //index is the non negative value which is smaller than the size of the vector
+    let index:usize = input1.trim().parse().expect("Invalid Input");
+
+    //getting value at given index value
+    let ch: Option<&char> = v.get(index);
+    value(ch);
+}
+
+
+
