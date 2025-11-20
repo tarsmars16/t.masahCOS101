8446a7092824a3bce207723cf939292d5517c04b
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-20 17:25:38 +0100

    Rust Slice

---
+++ week-8/practice_11/target/debug/deps/practice_11.d
@@ -0,0 +1,5 @@
+C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_11\target\debug\deps\practice_11.d: src\main.rs
+
+C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_11\target\debug\deps\practice_11.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_11\target\debug\practice_11.exe: C:\Users\HCC\Documents\t.masahCOS101\week-8\practice_11\src\main.rs
