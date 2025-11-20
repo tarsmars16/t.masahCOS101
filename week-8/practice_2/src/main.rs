2720bf7024f35acf82c717383b65229473c58a44
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-20 16:26:54 +0100

    Using Subsript Operator

--- week-8/practice_2/src/main.rs
+++ week-8/practice_2/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec!['C','O','M','P','U','T','E','R'];
+
+    let mut input1 = String::new();
+
+    println!("Enter an index value between (0 - 7)");
+    std::io::stdin().read_line(&mut input1).expect("Failed to Read Input");
+    //index is the on negative value which is smaller than the size of the vector
+    let index:usize = input1.trim().parse().expect("Invalid Input");
+
+    //getting value at given index value
+    let ch:char = v[index];
+
+
+    print!("{} is the character for index [{}]\n",ch, index);
 }
