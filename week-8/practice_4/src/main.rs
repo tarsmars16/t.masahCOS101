72a65fcf3772d8588ed1e73efd063d65af046a0b
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-20 16:38:18 +0100

    Iterating on the Vector

--- week-8/practice_4/src/main.rs
+++ week-8/practice_4/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    
+    //Name vector
+    let name = vec!["Mary","Sam","Sally","Greg","Ade","Mark","June","Ife"];
+
+    //Age Vector
+    let age = vec![16,17,19,22,20,21,18,23];
+
+    print!("\nAge Allocation\n");
+
+    //loop to iterate elements in vector
+    for i in 0..age.len() {
+        //iterating through i on the vector
+        print!("{} is {} years old\n",name[i],age[i]);
+    }
+
 }
