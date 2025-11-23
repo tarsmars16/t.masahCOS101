f209fc9df2ac185f462f5466defd5995f499115b
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-23 23:45:53 +0100

    EY Nigeria Experience Checker

--- week-8/project_2/src/main.rs
+++ week-8/project_2/src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     println!("              === EY Nigeria Experience Checker ===");
 
     loop {
-        println!("Enter The Name of the Developer:");
+        println!("\nEnter The Name of the Developer:");
         let mut input1 = String::new();
         io::stdin().read_line(&mut input1).expect("Failed to Read String");
         let name = input1.trim().to_string(); //user input for the name of the developer
 
         let mut input2 = String::new();
-        println!("Enter years of experience for {}:", name);
+        println!("\nEnter years of experience for {}:", name);
         io::stdin().read_line(&mut input2).expect("Failed to Read String");
         let experience: u32 = input2.trim().parse().expect("Invalid Input"); 
         
@@ -21,41 +21,43 @@ fn main() {
 
         // Ask if user wants to continue
         let mut input3 = String::new();
-        println!("Do you want to enter another developer? (y/n):");
+        println!("\nDo you want to enter another developer? (y/n):");
         io::stdin().read_line(&mut input3).expect("Failed to Read String");
         let choice = input3.trim().to_lowercase();
 
         if choice == "n" {
-            println!("Thank you for using EY Nigeria Experience Checker");
+            println!("\nThank you for using EY Nigeria Experience Checker");
             break; 
         }
         else if choice == "y" {
-            println!("Below are the Results");
+            println!("\nInput the Information of the Next Developer\n");
         }
         else { println!("Invalid Input. Try Again");
         continue; }
 
     }
-
+    //stops if no developers were entered
     if developers.is_empty() {
         println!("No developers entered.");
         return;
     }
 
-    let mut max_index = 0;
+    let mut max_index = 0; //representing the index of the developer with the highest experience.
 
-    for i in 1..developers.len() {
-        if developers[i].1 > developers[max_index].1 {
-        max_index = i; }
+    for i in 1..developers.len() { //Loop through all other developers
+        if developers[i].1 > developers[max_index].1 { //checks if experience is higher the current
+        max_index = i; } 
 
     }
 
-    let max_dev = &developers[max_index];
+    let highest_dev = &developers[max_index]; //stored in a variable
 
-    println!("Most experienced developer: {} with {} years", max_dev.0, max_dev.1);
+    println!("Most experienced developer: {} with {} years", highest_dev.0, highest_dev.1);
+
+    //Results
 
     println!("\n======== RESULT==========");
     println!("Most experienced developer:");
-    println!("Name: {}", max_dev.0);
-    println!("Experience: {} years", max_dev.1);
+    println!("Name: {}", highest_dev.0);
+    println!("Experience: {} years", highest_dev.1);
 }
