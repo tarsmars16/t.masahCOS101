99487bea457ce32f238dfb82cb67d9fb985a0380
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-11-26 16:37:40 +0100

    Opening and Reading Data from an Already Existing File

--- week-9/practice_2/src/main.rs
+++ week-9/practice_2/src/main.rs
@@ -3,7 +3,7 @@ use std::io::Read;
 
 fn main() {
 
-    let mut file = std::fs::File::open("welcome_message.txt").expect("Open Failed");
+    let mut file = std::fs::File::open("welcome_message.txt").expect("Open Operation Failed");
     let mut contents = String::new();
     file.read_to_string(& mut contents).expect("Read Failed");
     println!("{}",contents);
