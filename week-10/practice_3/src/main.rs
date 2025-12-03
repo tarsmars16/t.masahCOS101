86463ed33fe6243fef297fd3d106dbd00dbc218c
Masah Tare <tamaratare.masah@pau.edu.ng>
2025-12-03 16:30:10 +0100

    Returning value from a function using .clone()

---
+++ week-10/practice_3/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let v = vec![20,40,60,80];
+    //vector v owns the object in the heap
+
+    let v2 = v.clone();
+    let _v2_return = display(v2);
+    println!("In main {:?}",v);
+}
+fn display(v:Vec<i32>) -> Vec<i32> {
+    //returning same vector
+    println!("Inside display {:?} ",v);
+    return v;
+}
